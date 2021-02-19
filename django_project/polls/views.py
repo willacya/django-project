17cce19b10d7e1a0e3cc168ca18cb49db7c49e80
Andrew <andrew@x553ma.Home>
2021-02-19 06:54:06 +0000

    Added 404 page

--- django_project/polls/views.py
+++ django_project/polls/views.py
@@ -9,10 +9,14 @@ def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
 	template = loader.get_template('polls/index.html')
 	context = {'latest_question_list':latest_question_list,}
-	return HttpResponse(template.render(context, request))
+	return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-	return HttpResponse("You're looking at question %s." % question_id)
+	try:
+		question = Question.objects.get(pk = question_id):
+	except Question.DoesNotExist:
+		raise Http404("Question does not exist")
+	return render(request, 'polls/details.html', {'question':question})
 
 def results(request, question_id):
 	response = "You're looking at the results of question %s."
